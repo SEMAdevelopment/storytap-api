18503d58a1419607eb5d1251f84693925413426f
Kurt Nelson <kurtisnelson@gmail.com>
2013-07-12 16:18:24 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StorytapApi::Application.config.secret_key_base = 'f8a824db1078b6d5dc8b4afcee553a98fe2bafefba0d8a7739a9bed6d99d0bea35e11af67c0bc4210b8746989e8f62eb9efa1d970ce3e3bd0d3f5f06860c4c00'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+StorytapApi::Application.routes.draw do
+  root to: 'home#index'
+end
